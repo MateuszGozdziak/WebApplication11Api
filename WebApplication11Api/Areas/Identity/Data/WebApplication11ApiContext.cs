c38be09ee6e29eacfa6eee5706b750d2d5e8f2b2
MateuszGoździak <mateuszgozd@gmail.com>
2022-09-23 19:13:20 +0200

    Add project files.

---
+++ WebApplication11Api/Areas/Identity/Data/WebApplication11ApiContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication11Api.Data;
+
+public class WebApplication11ApiContext : IdentityDbContext<IdentityUser>
+{
+    public WebApplication11ApiContext(DbContextOptions<WebApplication11ApiContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
